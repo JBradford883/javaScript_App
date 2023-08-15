8dba5b71c5be4ad7e882550487f061f3668fffb5
Justin Bradford <jbradford883@optonline.net>
2023-08-15 14:26:06 -0400

    Create new variable with an array for pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert('Hello World');
-
-let favFood = 'Pizza';
-document.write(favFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        type: ['Grass', 'Poison'],
+        species: 'Seed',
+        height: 0.7
+    },
+    {
+        name: 'Charmander',
+        type: ['Fire'],
+        species: 'Lizard',
+        height: 0.6
+    },
+    {
+        name: 'Squirtle',
+        type: ['Water'],
+        species: 'Young Turtle',
+        height: 0.5
+    }
+]
